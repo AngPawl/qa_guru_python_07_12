4df440d5014dab651e3df9227d2e2649a5b60dff
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-09-04 19:58:05 +0300

    add jenkins fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
-from selene import browser
+
+from selene import browser, Config, Browser
+from selene.support import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -11,3 +14,24 @@ def open_new_browser():
     yield
 
     browser.quit()
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
